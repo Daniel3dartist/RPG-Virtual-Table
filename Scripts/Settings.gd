c74e22e519b7a1583bf65e98baffa17aa9e06142
Daniel3dartist <danielsobrinho3dartist@gmail.com>
2022-04-17 19:16:04 -0300

    Settings UI is functional

--- Scripts/Settings.gd
+++ Scripts/Settings.gd
@@ -4,6 +4,7 @@ var fpath = 'res://Saves_Teste/Settings.json'
 
 var types_of_windows_modes = ['Window', 'Full Screen', 'Borderless Window']
 var resolutions = ['640 x 480', '800 x 600', '1024 x 768', '1152 x 864','1280 x 720', '1280 x 800', '1280 x 1024', '1366 x 768', '1440 x 900', '1600 x 900', '1680 x 1050', '1920 x 1080', '2560 x 1440', '2048 x 1080', '3840 x 2160', '7680 x 4320']
+
 var tkey 
 var rkey
 var settings_keys
@@ -49,8 +50,9 @@ func check_settings_file():
 	var ftest
 	var f
 	var content
-	
+
 	ftest = fpathtest.file_exists(fpath)
+
 	if ftest != true:
 		print('Creating a new settings file ')
 		fpathtest.close()
@@ -59,6 +61,7 @@ func check_settings_file():
 		windowsl.text = types_of_windows_modes[tkey]
 		resolutionsl.text = resolutions[rkey]
 		creat_file()
+		windows_mod()
 	else:
 		print('Settings file allready exists')
 		f = File.new()
@@ -71,11 +74,13 @@ func check_settings_file():
 		windowsl.text = types_of_windows_modes[tkey]
 		resolutionsl.text = resolutions[rkey]
 		f.close()
-		
+
 		xy = resolutions[int(content[1])].split('x')
 		vx = xy[0]
 		vy = xy[1]
 		OS.window_size = Vector2(vx, vy)
+		
+		windows_mod()
 
 
 # Window modes
@@ -100,7 +105,7 @@ func _on_WFront_button_up():
 	var k = types_of_windows_modes[tkey]
 	
 	print(k)
-	if k != types_of_windows_modes[2]:
+	if k != types_of_windows_modes[-1]:
 		tkey = tkey + 1
 		windowsl.text = twindow[tkey]
 		print(str(tkey))
@@ -137,7 +142,7 @@ func _on_RFront_button_up():
 	print('Essa é a lista de keys: ' + str(screen.size()))
 	
 	print(k)
-	if k != resolutions[0]:
+	if k != resolutions[-1]:
 		rkey = rkey + 1
 		resolutionsl.text = screen[rkey]
 		print(screen[rkey])
@@ -157,14 +162,17 @@ func _on_Save_Settings_button_up():
 	t = true
 	timer.start()
 	OS.window_size = Vector2(vx, vy)
-#	viewport.set_size_override(true, Vector2(vx, vy))
+	windows_mod()
 
 # Restore Settings
 func _on_Restore_button_up():
 	restore()
+	
 	t = false
 	timer.stop()
 	popup.visible = false
+	
+	windows_mod()
 
 
 func creat_file():
@@ -175,6 +183,21 @@ func creat_file():
 	f.store_string(to_json(settings_keys))
 	print('"settings.json" created')
 	f.close()
+
+
+func _on_Accepted_button_up():
+	t = false
+	timer.stop()
+	popup.visible = false
+	save()
+	windows_mod()
+
+
+func _on_Restore_Last_button_up():
+	check_settings_file()
+	t = false
+	timer.stop()
+	popup.visible = false
 
 
 func save():
@@ -193,6 +216,7 @@ func save():
 	f.store_string(to_json(txt))
 	print('User_config.json updated \n', 'The new string is: ', txt, '\n')
 	f.close()
+	
 
 
 func restore():
@@ -207,17 +231,21 @@ func restore():
 	rkey = 4
 	save()
 
+func windows_mod():
+	var window = types_of_windows_modes[tkey]
+	
+	if window == 'Window':
+		OS.window_fullscreen = false
+		OS.window_borderless = false
+		OS.window_maximized = false
 
+	elif window == 'Full Screen':
+		OS.window_fullscreen = true
+		OS.window_borderless = false
+		OS.window_maximized = false
+		
+	elif window == 'Borderless Window':
+		OS.window_fullscreen = false
+		OS.window_borderless = true
+		OS.window_maximized = true
 
-func _on_Accepted_button_up():
-	t = false
-	timer.stop()
-	popup.visible = false
-	save()
-
-
-func _on_Restore_Last_button_up():
-	check_settings_file()
-	t = false
-	timer.stop()
-	popup.visible = false
