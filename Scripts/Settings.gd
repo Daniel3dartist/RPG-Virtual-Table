13445106284eb376d0bc61bf142337c5b87e8a9e
Daniel3dartist <danielsobrinho3dartist@gmail.com>
2022-05-08 00:11:53 -0300

    Refactoring Settings filers save code

--- Scripts/Settings.gd
+++ Scripts/Settings.gd
@@ -1,12 +1,15 @@
 extends Control
 
-var fpath = 'res://data/Settings.json'
 
-var types_of_windows_modes = ['Window', 'Full Screen', 'Borderless Window']
-var resolutions = ['640 x 480', '800 x 600', '1024 x 768', '1152 x 864','1280 x 720', '1280 x 800', '1280 x 1024', '1366 x 768', '1440 x 900', '1600 x 900', '1680 x 1050', '1920 x 1080', '2560 x 1440', '2048 x 1080', '3840 x 2160', '7680 x 4320']
+var fpath = "res://data/Settings.cfg"
 
-var tkey 
-var rkey
+var types_of_windows_modes = ["Window", "Full Screen", "Borderless Window"]
+var resolutions = ["640 x 480", "800 x 600", "1024 x 768", "1152 x 864", "1280 x 720", "1280 x 800", "1280 x 1024", "1366 x 768", "1440 x 900", "1600 x 900", "1680 x 1050", "1920 x 1080", "2560 x 1440", "2048 x 1080", "3840 x 2160", "7680 x 4320"]
+
+var d
+
+var Wkey 
+var Rkey
 var settings_keys
 var t
 var reskey
@@ -29,6 +32,7 @@ onready var popup = $'PopupMenu'
 func _ready():
 	check_settings_file()
 	timer.set_wait_time(15)
+	popup.visible = false
 
 
 func _process(delta):
@@ -36,9 +40,7 @@ func _process(delta):
 		timer.set_one_shot(true)
 		count.text = str(int(timer.get_time_left()))
 		popup.visible = true
-#		print(count.text)
 	if int(timer.get_time_left()) == 0 and t == true:
-		print('Acabou o tempo')
 		popup.visible = false
 		restore()
 		timer.stop()
@@ -46,116 +48,110 @@ func _process(delta):
 
 
 func check_settings_file():
-	var fpathtest = File.new()
-	var ftest
-	var f
-	var content
+	var config = ConfigFile.new()
+	var ftest = File.new()
+	var err = config.load(fpath)
+	
+	if err != OK:
+		print('Erro ao ler arquivo')
+		ftest = ftest.file_exists(fpath)
+		if ftest != true:
+			config.set_value("Video", "Window", "Window")
+			config.set_value("Video", "Resolution", "1280 x 720")
+			config.save(fpath)
 
-	ftest = fpathtest.file_exists(fpath)
+	for Video in config.get_sections():
+		Wkey = config.get_value(Video, "Window")
+		Rkey = config.get_value(Video, "Resolution")
 
-	if ftest != true:
-		print('Creating a new settings file ')
-		fpathtest.close()
-		tkey = 0
-		rkey = 4
-		windowsl.text = types_of_windows_modes[tkey]
-		resolutionsl.text = resolutions[rkey]
-		creat_file()
-		windows_mod()
-	else:
-		print('Settings file allready exists')
-		f = File.new()
-		f.open(fpath, File.READ)
-		content = f.get_as_text()
-		content = parse_json(content)
-		print('Esse é o content: ' + str(content[1]))
-		tkey = int(content[0])
-		rkey = int(content[1])
-		windowsl.text = types_of_windows_modes[tkey]
-		resolutionsl.text = resolutions[rkey]
-		f.close()
-
-		xy = resolutions[int(content[1])].split('x')
-		vx = xy[0]
-		vy = xy[1]
-		OS.window_size = Vector2(vx, vy)
-		
-		windows_mod()
-
+	windowsl.text = Wkey
+	resolutionsl.text = Rkey
+	xy = Rkey.split('x')
+	vx = int(xy[0])
+	vy = int(xy[1])
+	OS.window_size = Vector2(vx, vy)
+	print(Wkey, Rkey)
+	windows_mod()
 
 # Window modes
 func _on_WBack_button_up():
 	var twindow = types_of_windows_modes
-	var k = types_of_windows_modes[tkey]
+	var k = twindow.find(Wkey)
+	var sz = twindow.size() - 1
+	var w
 	
-	print(types_of_windows_modes[tkey])
-	if k != types_of_windows_modes[0]:
-		tkey = tkey - 1
-		windowsl.text = str(twindow[tkey])
-		print(str(tkey))
+	print(twindow.size())
+	if k <= sz:
+		k -= 1
+		w = twindow[k]
+		Wkey = w
+		windowsl.text = w
+		print(k)
 	else:
-		tkey = -1
-		print(tkey)
-		windowsl.text = twindow[tkey]
-		print(twindow[tkey])
+		k = sz
+		w = twindow[k]
+		Wkey = w
+		windowsl.text = w
 
 
 func _on_WFront_button_up():
 	var twindow = types_of_windows_modes
-	var k = types_of_windows_modes[tkey]
-	
-	print(k)
-	if k != types_of_windows_modes[-1]:
-		tkey = tkey + 1
-		windowsl.text = twindow[tkey]
-		print(str(tkey))
-	else:
-		tkey = 0
-		print(tkey)
-		windowsl.text = twindow[tkey]
-		print(twindow[tkey])
+	var k = twindow.find(Wkey)
+	var sz = twindow.size() - 1
+	var w
 
+	if k < sz:
+		k += 1
+		w = twindow[k]
+		Wkey = w
+		windowsl.text = w
+		print(k)
+	else:
+		k = 0
+		w = twindow[k]
+		Wkey = w
+		windowsl.text = w
+		print(k)
 
 # Resolutions
 func _on_RBack_button_up():
 	var screen = resolutions
-	var k = resolutions[rkey]
-	
-	print('Essa é a lista de keys: ' + str(screen.size()))
-	
-	print(k)
-	if k != resolutions[0]:
-		rkey = rkey - 1
-		resolutionsl.text = screen[rkey]
-		print(screen[rkey])
+	var k = screen.find(Rkey)
+	var sz = screen.size() - 1
+	var r
+
+	if k > 0:
+		k -= 1
+		r = screen[k]
+		Rkey = r
+		resolutionsl.text = r
 	else:
-		rkey = -1
-		resolutionsl.text = screen[rkey]
-		print("Rkey é " + str(rkey))
-		print("Resolutions.text é: " + resolutionsl.text)
+		k = -1
+		r = screen[k]
+		Rkey = r
+		resolutionsl.text = r
 
 
 func _on_RFront_button_up():
 	var screen = resolutions
-	var k = resolutions[rkey]
-	
-	print('Essa é a lista de keys: ' + str(screen.size()))
-	
-	print(k)
-	if k != resolutions[-1]:
-		rkey = rkey + 1
-		resolutionsl.text = screen[rkey]
-		print(screen[rkey])
-	else:
-		rkey = 0
-		resolutionsl.text = screen[rkey]
-		print("Rkey é " + str(rkey))
-		print("Resolutions.text é: " + resolutionsl.text)
+	var k = screen.find(Rkey)
+	var sz = screen.size() - 1
+	var r
 
+	if k != sz:
+		k = k + 1
+		r = screen[k]
+		Rkey = r
+		resolutionsl.text = r
+	else:
+		k = 0
+		r = screen[k]
+		Rkey = r
+		resolutionsl.text = r
 
 # Save Settings
 func _on_Save_Settings_button_up():
-	xy = resolutions[rkey].split('x')
+	xy = Rkey.split('x')
 	vx = int(xy[0])
 	vy = int(xy[1])
 	
@@ -167,22 +163,10 @@ func _on_Save_Settings_button_up():
 # Restore Settings
 func _on_Restore_button_up():
 	restore()
-	
 	t = false
 	timer.stop()
 	popup.visible = false
-	
 	windows_mod()
-
-
-func creat_file():
-	var f = File.new()
-	
-	settings_keys = [0, 4]
-	f.open(fpath, File.WRITE)
-	f.store_string(to_json(settings_keys))
-	print('"settings.json" created')
-	f.close()
 
 
 func _on_Accepted_button_up():
@@ -201,38 +185,29 @@ func _on_Restore_Last_button_up():
 
 
 func save():
-	var f = File.new()
-	var txt
+	var config = ConfigFile.new()
 	
-	f.open(fpath, File.READ)
-	txt = f.get_as_text()
-	txt = parse_json(txt)
-	
-	txt[0] = tkey
-	txt[1] = rkey
-	print('Esse é o TXT com a mudança das Keys: ', txt)
-	
-	f.open(fpath, File.WRITE)
-	f.store_string(to_json(txt))
-	print('User_config.json updated \n', 'The new string is: ', txt, '\n')
-	f.close()
-	
+	config.set_value("Video", "Window", Wkey)
+	config.set_value("Video", "Resolution", Rkey)
+	config.save(fpath)
 
 
 func restore():
-	xy = resolutions[4].split('x')
+	Wkey = 'Window'
+	Rkey = '1280 x 720'
+	
+	xy = Rkey.split('x')
 	vx = int(xy[0])
 	vy = int(xy[1])
 	
 	OS.window_size = Vector2(vx, vy)
-	windowsl.text = types_of_windows_modes[0]
-	resolutionsl.text = resolutions[4]
-	tkey = 0
-	rkey = 4
+	windows_mod()
+	windowsl.text = Wkey
+	resolutionsl.text = Rkey
 	save()
 
 func windows_mod():
-	var window = types_of_windows_modes[tkey]
+	var window = Wkey
 	
 	if window == 'Window':
 		OS.window_fullscreen = false
