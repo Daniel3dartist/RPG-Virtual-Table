06f6e99d94086f2cd645030b601331775fdbc6d2
Daniel3dartist <danielsobrinho3dartist@gmail.com>
2022-12-25 21:45:21 -0300

    Prototyping token modularity

---
+++ Scenes/Sheet/TextureRect.gd
@@ -0,0 +1,13 @@
+extends TextureRect
+
+
+func _input(event):
+	if Input.is_action_just_pressed("ui_down"):
+		var texture = load('res://Krita_Imagens/Alpha token prototype_Mask_Area.png')
+#		print(self.ShaderMaterial.get_node_list())
+		self.material.set_shader_param("tex_frg_2", texture)
+
+	if Input.is_action_just_pressed("enter"):
+		var texture = load('res://Krita_Imagens/Orc_do_Zé_01.jpg')
+#		print(self.ShaderMaterial.get_node_list())
+		self.material.set_shader_param("tex_frg_2", texture)
